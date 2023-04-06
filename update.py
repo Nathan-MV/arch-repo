134691472f271feabfa425e7bc314ce346cc9bd7
Nathan-MV <nathanmvieira@outlook.com>
2023-04-06 18:35:28 -0300

    fix

--- update.py
+++ update.py
@@ -1,11 +1,8 @@
 import os
 import re
 import requests
-import shutil
-import subprocess
 import hashlib
 import glob
-import concurrent.futures
 from tqdm import tqdm
 
 def download_file(url, filepath):
@@ -15,16 +12,12 @@ def download_file(url, filepath):
         file_size = int(response.headers.get("content-length", 0))
         chunk_size = 4 * 1024 * 1024  # 1 MB chunks
         with open(filepath, "wb") as f, tqdm(total=file_size, unit="B", unit_scale=True, desc=filepath) as progress_bar:
-            with concurrent.futures.ThreadPoolExecutor() as executor:
-                futures = []
-                for chunk in response.iter_content(chunk_size=chunk_size):
-                    future = executor.submit(f.write, chunk)
-                    futures.append(future)
-                for future in concurrent.futures.as_completed(futures):
-                    future.result()
-                    progress_bar.update(chunk_size)
+            downloaded = 0
+            for chunk in response.iter_content(chunk_size=chunk_size):
+                f.write(chunk)
+                downloaded += len(chunk)
+                progress_bar.update(len(chunk))
 
-updated_pkgs = set()
 for filepath in glob.glob(os.path.join("packages", "**", "PKGBUILD")):
     print(filepath)
     with open(filepath, "r") as f:
@@ -55,12 +48,6 @@ for filepath in glob.glob(os.path.join("packages", "**", "PKGBUILD")):
             with open("PKGBUILD", "w") as f:
                 f.write(re.sub(r"pkgver=(.*)", f"pkgver={tag}", re.sub(r"sha256sums=\('(.*)'\)", f"sha256sums=('{pkg_hash}')", contents)))
                 print(f"PKGBUILD updated to {tag} for {project}")
-            updated_pkgs.add(project)
         else:
             print(f"No updates found on GitLab or the PKGBUILD file is already up-to-date for {project}.")
         os.chdir("../../")
-
-if updated_pkgs:
-    print(f"Updated PKGBUILD files for: {', '.join(updated_pkgs)}")
-else:
-    print("No updates found on GitLab or all PKGBUILD files are already up-to-date.")
