f1a58353b662fe971a4d17a136f218403b8c393d
Nathan-MV <nathanmvieira@outlook.com>
2023-04-07 00:30:08 -0300

    github integration, error handling

--- PKGBUILD
+++ PKGBUILD
@@ -1,44 +1,24 @@
 # Maintainer: Peter Nokes <peter@peternokes.co.uk>
 
-pkgname=komikku
+pkgname=Komikku
 _author=valos
 _gitname=Komikku
-pkgver=v1.15.0
+pkgver=v1.17.1
 pkgrel=2
 pkgdesc='Online/Offline Manga reader based on GNOME | PinePhone/Librem 5 Support'
 arch=(any)
 url=https://gitlab.com/valos/Komikku
 license=(GPL3)
 depends=(
-  libadwaita
-  python-beautifulsoup4
-  python-brotli
-  python-cairo
-  python-cffi
-  python-cloudscraper
-  python-dateparser
-  python-emoji
-  python-gobject
-  python-keyring
-  python-lxml
-  python-magic
-  python-natsort
-  python-pillow
-  python-piexif
-  python-pure-protobuf
-  python-unidecode
-  python-wheel
-  python-rarfile
-  webkit2gtk
-  webkit2gtk-5.0
+  'libadwaita' 'python-beautifulsoup4' 'python-brotli' 'python-cairo' 'python-cffi'
+  'python-cloudscraper' 'python-dateparser' 'python-emoji' 'python-gobject' 'python-keyring'
+  'python-lxml' 'python-magic' 'python-natsort' 'python-pillow' 'python-piexif' 'python-pure-protobuf'
+  'python-unidecode' 'python-wheel' 'python-rarfile' 'webkit2gtk' 'webkit2gtk-5.0'
 )
-makedepends=(
-  gobject-introspection
-  meson
-)
+makedepends=('gobject-introspection' 'meson')
 optdepends=('org.freedesktop.secrets: store passwords safely')
 source=("https://gitlab.com/$_author/$_gitname/-/archive/v$pkgver/$_gitname-v$pkgver.tar.gz")
-sha256sums=('None')
+sha256sums=('4bcd96fc52b1e3826bf6df54817b0a57402f6d0fa03ce8252c1768cf1cf8cfa0')
 
 build() {
   arch-meson $_gitname-v$pkgver build
--- update.py
+++ update.py
@@ -4,7 +4,6 @@ import requests
 import hashlib
 import glob
 from tqdm import tqdm
-import git
 from contextlib import closing
 
 def download_file(url: str, filepath: str) -> None:
@@ -17,38 +16,62 @@ def download_file(url: str, filepath: str) -> None:
                 f.write(chunk)
                 progress_bar.update(len(chunk))
 
+https = "https://"
 for filepath in glob.glob(os.path.join("packages", "**", "PKGBUILD")):
     print(filepath)
     with open(filepath, "r") as f:
         contents = f.read()
-    if url_match := re.search(r"url=(https://gitlab.com/[^\n]*)", contents):
-        gitlab_url = url_match[1]
-        parts = gitlab_url.split("/", 4)
-        url = "/".join(parts[:-2])
-        user, project = parts[-2:]
+    if source_match := re.search(r'source=\("(https?://(?:www\.)?(github|gitlab).com/[^\n]*")\)', contents):
+        full_source = source_match[1].split("/")
+        source = "/".join(full_source[2:3])
+
+        print(full_source)
+        print(source)
+    else:
+        print("source didnt match")
+        break
+
+    if re.search(r"(gitlab|github).com", source):
+        author = full_source[3]
+        print(author)
+    else:
+        print("author didnt match")
+        break
+
+    if pkg_name_match := re.search(r"(pkgname|pkgbase)=([^\n]*)", contents):
+        pkg_name = pkg_name_match[2]
+        print(pkg_name + "\n")
+    else:
+        print("pkgname didnt match")
+        break
+
+    if re.search(r"gitlab.com", source):
+        response = requests.get(f"{https}{source}/api/v4/projects/{author}%2F{pkg_name}/releases")
+    elif re.search(r"github.com", source):
+        response = requests.get(f"{https}api.{source}/repos/{author}/{pkg_name}/releases")
+    else:
+        print("no api response")
+        break
+
+    response.raise_for_status()
+    tag = response.json()[0]["tag_name"]
+    if tag != re.search(r"pkgver=(.*)", contents)[1]:
         os.chdir(os.path.dirname(filepath))
-        repo = git.Repo(search_parent_directories=True)
-        gitlab_url = f"{url}/{user}/{project}.git"
-        tag = repo.git.ls_remote(gitlab_url, "refs/tags/*", sort="-v:refname", exit_code=True).split("\n")[0].split("refs/tags/")[1]
-        print(f"New version found: {tag}")
-        if tag != re.search(r"pkgver=(.*)", contents)[1]:
-            url = f"{url}/{user}/{project}/-/archive/{tag}/{project}-{tag}.tar.gz"
-            response = requests.head(url)
-            response.raise_for_status()
-            pkg_url = response.headers["ETag"]
-            pkg_hash = ""
-            if os.path.exists(f"{project}-{tag}.tar.gz"):
-                with open(f"{project}-{tag}.tar.gz", "rb") as f:
-                    file_hash = hashlib.sha256(f.read()).hexdigest()
-                if file_hash == pkg_url:
-                    pkg_hash = file_hash
-            if not pkg_hash:
-                download_file(url, f"{project}-{tag}.tar.gz")
-                with open(f"{project}-{tag}.tar.gz", "rb") as f:
-                    pkg_hash = hashlib.sha256(f.read()).hexdigest()
-            with open("PKGBUILD", "w") as f:
-                f.write(re.sub(r"pkgver=(.*)", f"pkgver={tag}", re.sub(r"sha256sums=\('(.*)'\)", f"sha256sums=('{pkg_hash}')", contents)))
-                print(f"PKGBUILD updated to {tag} for {project}")
+        if re.search(r"gitlab.com", source):
+            url = f"{https}{source}/{author}/{pkg_name}/-/archive/{tag}/{pkg_name}-{tag}.tar.gz"
+        elif re.search(r"github.com", source):
+            url = f"{https}{source}/{author}/{pkg_name}/archive/refs/tags/{tag}.tar.gz"
         else:
-            print("No updates found for the PKGBUILD's.")
-        os.chdir("../../")
+            print("no tar.gz found")
+            break
+        if not os.path.exists(f"{pkg_name}-{tag}.tar.gz"):
+            download_file(url, f"{pkg_name}-{tag}.tar.gz")
+        with open(f"{pkg_name}-{tag}.tar.gz", "rb") as f:
+            pkg_hash = hashlib.sha256(f.read()).hexdigest()
+        with open("PKGBUILD", "w") as f:
+            f.write(re.sub(r"pkgver=(.*)", f"pkgver={tag}", re.sub(r"sha256sums=\('(.*)'\)", f"sha256sums=('{pkg_hash}')", contents)))
+            print(f"PKGBUILD updated to {tag} for {pkg_name}")
+    else:
+        print("No updates found for the PKGBUILD's.")
+        continue
+    os.chdir("../../")
